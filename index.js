26a5d68d33cfa4ef31ca64d26720d79c4f4e9644
ODude Network <web3yak@gmail.com>
2024-04-30 23:04:20 +0545

    1

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fa = require("@glif/filecoin-address");
 var _ethers = require("ethers");
 const w3d = require("@odude/odudename");
-require('dotenv').config() //Remove this line if no environment variable is used
-
-//nodemon app.js
+const fa = require("@glif/filecoin-address");
+require("dotenv").config();
 
 const settings = {
   matic_rpc_url: process.env.MATIC_RPC,
@@ -16,22 +14,26 @@ const settings = {
 
 let resolve = new w3d(settings);
 
-var intro = "This is live API script which you can host on your node webserver application to get wallet address from the Web3 Domain Name.<hr> Eg. <code>http://....../api/?name=brad.eth&#38;currency=ETH</code> ";
+var intro =
+  "This is live API script which you can host on your node webserver application to get wallet address from the Web3 Domain Name.<hr> Eg. <code>http://....../?name=brad.eth&currency=ETH</code> ";
 
-app.get('/', (req, res) => {
-  res.send(intro);
+
+  
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-//Change port number as to your server
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
-});
 
-app.get("/api", (req, res) => {
-  let query = req.query
+// Define routes
+app.get("/", (req, res) => {
+  const { param } = req.query;
+  let query = req.query;
+
   console.log(query);
 
-
+  
   if ((typeof query.address !== 'undefined')) {
 
     //Search for ETH address
@@ -82,97 +84,87 @@ app.get("/api", (req, res) => {
 
     }
     else {
-      res.json({ error: 'Must define [name] & [currency] parameters' })
+      res.json({ error: intro })
     }
   }
 
-
-
-})
+});
 
 //Domain to Address
 function domain_to_addr(name, currency, res) {
-  resolve.getAddress(name, currency).then(x => {
-    console.log(x);
-    if (x == null) {
-      res.json({ address: x, code: 404 })
-    }
-    else {
-      res.json({ address: x, code: 200 })
-    }
-  }).catch(console.error);
-
+  resolve
+    .getAddress(name, currency)
+    .then((x) => {
+      console.log(x);
+      if (x == null) {
+        res.json({ address: x, code: 404 });
+      } else {
+        res.json({ address: x, code: 200 });
+      }
+    })
+    .catch(console.error);
 }
 
 //Address to Domain
 
 function addr_to_domain(address, res) {
-
- // const convert_t4 = fa.delegatedFromEthAddress(address).toString();
-
   const convert_f4 = fa.newDelegatedEthAddress(address).toString();
   console.log(convert_f4);
 
-  //convert to f2
- // const convert_ac = fa.newActorAddress(address).toString();
-//  console.log(convert_ac);
-
-  //const convert_bb = fa.newSecp256k1Address(convert_f4).toString();
-  //console.log(convert_bb);
-
-  //const convert_cc = fa.newBLSAddress(address).toString();
-  //console.log(convert_cc);
-
-  resolve.getDomain(address, "W3D").then(x => {
-    //EVM address to Web3Domain Name
-    if (x == null || x == '') {
-
-      addr_to_domain_ens(address, res);
-    }
-    else {
-      res.json({ domain: x, code: 200, fvm: convert_f4, eth: address })
-    }
-  }).catch(console.error);
+  resolve
+    .getDomain(address, "W3D")
+    .then((x) => {
+      //EVM address to Web3Domain Name
+      if (x == null || x == "") {
+        addr_to_domain_ens(address, res);
+      } else {
+        res.json({ domain: x, code: 200, fvm: convert_f4, eth: address });
+      }
+    })
+    .catch(console.error);
 }
 
 //Address to domain for ENS
 function addr_to_domain_ens(address, res) {
-
   const convert_t4 = fa.delegatedFromEthAddress(address).toString();
 
-  resolve.getDomain(address, "ENS").then(x => {
-    //ENS address to ETH Domain
-    if (x == null) {
-      res.json({ domain: x, code: 404 })
-    }
-    else {
-      res.json({ domain: x, code: 200, fvm: convert_t4, eth: address })
-    }
-  }).catch(console.error);
+  resolve
+    .getDomain(address, "ENS")
+    .then((x) => {
+      //ENS address to ETH Domain
+      if (x == null) {
+        res.json({ domain: x, code: 404 });
+      } else {
+        res.json({ domain: x, code: 200, fvm: convert_t4, eth: address });
+      }
+    })
+    .catch(console.error);
 }
 
 //Find IPFS from domain
 function domain_to_ipfs(name, res) {
-
-  resolve.getWeb(name).then(x => {
-    if (x == null) {
-      res.json({ ipfs: x, code: 404 })
-    }
-    else {
-      res.json({ ipfs: x, code: 200 })
-    }
-  }).catch(console.error);
+  resolve
+    .getWeb(name)
+    .then((x) => {
+      if (x == null) {
+        res.json({ ipfs: x, code: 404 });
+      } else {
+        res.json({ ipfs: x, code: 200 });
+      }
+    })
+    .catch(console.error);
 }
 
 //Find URI from domain
 function domain_to_uri(name, res) {
-
-  resolve.w3d_tokenURI(name).then(x => {
-    if (x == null) {
-      res.json({ tokenURI: x, code: 404 })
-    }
-    else {
-      res.json({ tokenURI: x, code: 200 })
-    }
-  }).catch(console.error);
+  resolve
+    .w3d_tokenURI(name)
+    .then((x) => {
+      if (x == null) {
+        res.json({ tokenURI: x, code: 404 });
+      } else {
+        res.json({ tokenURI: x, code: 200 });
+      }
+    })
+    .catch(console.error);
 }
